988c0b7929179e64e9b766d40eb4d8de170d34dd
Ashley Claiborne <languageisaliquid@gmail.com>
2020-03-21 12:02:12 -0500

    Add project files.

---
+++ AnythingButCreed/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnythingButCreed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Queen = new Song("Queen", "Don't Stop Me Now");
+            var MollyParden = new Song("Molly Parden", "Losing My Religion");
+            var ModestMouse = new Song("Modest Mouse", "Cowboy Dan");
+            var MotherMother = new Song("Mother Mother", "Baby Don't Dance");
+            var Creed = new Song("Creed", "Arms Wide Open");
+            var AlsoCreed = new Song("Creed", "Something Else From Creed");
+            var Ciara = new Song("Ciara", "Like a Boy");
+            var HER = new Song("H.E.R.", "Focus");
+            var StephenMarley = new Song("Stephen Marley", "You're Gonna Leave");
+            var StMotel = new Song("Saint Motel", "My Type");
+            var Starbenders = new Song("Starbenders", "Blood");
+            var Pokey = new Song("Pokey LaFarge", "Something in the Water");
+
+            var goodSongs = new List<Song>();
+            var allSongs = new List<Song>() 
+            { 
+                Queen,
+                MollyParden, 
+                ModestMouse, 
+                MotherMother, 
+                Creed, 
+                AlsoCreed, 
+                Ciara, 
+                HER, 
+                Starbenders, 
+                StephenMarley, 
+                StMotel, 
+                Pokey
+            };
+
+            goodSongs = allSongs.Where(Song => Song.Artist != "Creed").ToList();
+
+            foreach (var song in goodSongs)
+            {
+                Console.WriteLine($"{song.Artist} : {song.Name}");
+            }
+        }
+    }
+}
